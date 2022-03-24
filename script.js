37eef96568cad42065f53b7951b251d81320d25c
Natasha Safronova <natasha.safrronova@gmail.com>
2022-03-24 21:19:57 +0300

    first homework

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = 'firstHomework';
+let screens;
+const screenPrice = 2000;
+let rollback = 10;
+let fullPrice = screenPrice + rollback;
+let adaptive;
+
+alert('Привет, я алерт!');
+console.log('Привет, я консоль!');
