c417762d347d078d6a72cb28a98aa60fa056e064
Natasha Safronova <natasha.safrronova@gmail.com>
2022-03-25 13:51:19 +0300

    задание к 02lesson

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let title = 'firstHomework';
-let screens;
-const screenPrice = 2000;
-let rollback = 10;
-let fullPrice = screenPrice + rollback;
-let adaptive;
-
-alert('Привет, я алерт из другой ветки!');
-console.log('Привет, я консоль!');
+let title = 'Second Homework';
+console.log(typeof title);
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 300;
+let rollback = 25;
+let fullPrice = 4000;
+console.log(typeof fullPrice);
+let adaptive = true;
+console.log(typeof adaptive);
