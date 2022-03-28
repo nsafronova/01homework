bcb942dceb77e822b6adbc103f8f9f0e3e20b387
Natasha Safronova <natasha.safrronova@gmail.com>
2022-03-28 13:31:18 +0300

    Динамическая типизация данных, условия, ветвления

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
-let title = 'Second Homework';
-console.log(typeof title);
+'use strict';
+
+let title = 'Third Homework';
 let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 300;
 let rollback = 25;
 let fullPrice = 4000;
-console.log(typeof fullPrice);
 let adaptive = true;
+let rollbackPrecent = fullPrice * (rollback / 100);
+
+console.log(typeof title);
+console.log(typeof fullPrice);
 console.log(typeof adaptive);
 
 console.log(screens.length);
 console.log(`Стоимость верстки экранов ${screenPrice} долларов\nСтоимость разработки сайта ${fullPrice} долларов`);
 
 console.log(screens.toLowerCase().split(', '));
-console.log(`Процент отката посреднику за работу ${fullPrice * (rollback/100)} долларов`);
+console.log(`Процент отката посреднику за работу ${rollbackPrecent} долларов`);
+
+
+// title = prompt('Как называется ваш проект?');
+// screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+// screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
+// adaptive = confirm('Нужен ли адаптив на сайте?');
+
+// let service1 = prompt('Какой дополнительный тип услуги нужен?');
+// let servicePrice1 = +prompt('Сколько это будет стоить?');
+
+// let service2 = prompt('Какой дополнительный тип услуги нужен?');
+// let servicePrice2 = +prompt('Сколько это будет стоить?');
+
+// fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+// let servicePercentPrice = +Math.ceil(fullPrice - rollbackPrecent);
+// console.log(servicePercentPrice);
+
+
+
+// if (fullPrice >= 30000) {
+//   console.log('Даем скидку в 10%');
+// } else if (fullPrice >= 15000) {
+//   console.log('Даем скидку в 5%');
+// } else if (fullPrice >= 0) {
+//   console.log('Скидка не предусмотрена');
+// } else {
+//   console.log('Что то пошло не так');
+// }
