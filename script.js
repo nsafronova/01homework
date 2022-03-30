39cbcd96e7ed95d77268c0a5b8004ab5803d80e1
Natasha Safronova <natasha.safrronova@gmail.com>
2022-03-30 23:12:29 +0300

    Циклы. Рекурсия.

--- script.js
+++ script.js
@@ -1,71 +1,76 @@
 'use strict';
 //блок объявления переменных
-let rollback = 25;
-let fullPrice = 400;
-
 let title = prompt('Как называется ваш проект?');
 let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
 let screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
 let adaptive = confirm('Нужен ли адаптив на сайте?');
-let rollbackPrecent = (fullPrice * (rollback / 100));
 
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
 
-let servicePercentPrice = +Math.ceil(fullPrice - rollbackPrecent);
-
+let fullPrice;
+let servicePercentPrice;
 let allServicePrices;
+let rollback = 25;
 
 //блок описания функций
 
-let getAllServicePrices = function (servicePrice1, servicePrice2) {
-
-  allServicePrices = servicePrice1 + servicePrice2;
-  console.log(allServicePrices);
+let getAllServicePrices = function () {
+  if (typeof servicePrice1 !== 'number') {
+    alert('Неверный тип данных!');
+  } else if (typeof servicePrice2 !== 'number') {
+    alert('Неверный тип данных!');
+  } else {
+    return servicePrice1 + servicePrice2;
+  }
 };
 
 
-function getFullPrice(screenPrice, allServicePrices) {
-  fullPrice = screenPrice + allServicePrices;
-  console.log(fullPrice);
+function getFullPrice() {
+  return screenPrice + allServicePrices;
 }
 
+let getServicePercentPrices = function () {
+  return fullPrice - (fullPrice * (rollback / 100));
+};
 
-
-function getTitle(title) {
-
-  title = title.trim().toLowerCase();
-  return title[0].toUpperCase();
+function getTitle() {
+  return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase();
 }
 
 //функциональный блок
 
 function getRollbackMessage() {
   if (fullPrice >= 30000) {
-    console.log('Даем скидку в 10%');
-  } else if (fullPrice >= 15000) {
-    console.log('Даем скидку в 5%');
-  } else if (fullPrice >= 0) {
-    console.log('Скидка не предусмотрена');
+    return 'Даем скидку в 10%';
+  } else if (fullPrice >= 15000 && fullPrice < 30000) {
+    return 'Даем скидку в 5%';
+  } else if (fullPrice >= 0 && fullPrice < 15000) {
+    return 'Скидка не предусмотрена';
   } else {
-    console.log('Что то пошло не так');
+    return 'Что то пошло не так';
   }
 }
 
-let getServicePercentPrices = function (fullPrice, rollback) {
-  servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
-  console.log(servicePercentPrice);
-};
-
 //мусорный блок
 
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices();
+title = getTitle();
+
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(getRollbackMessage(fullPrice));
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
-console.log(screens.toLowerCase().split(', '));
-console.log(getTitle(title));
 
-getRollbackMessage();
-getServicePercentPrices(fullPrice, rollback);
+console.log(screens.length);
+console.log(servicePercentPrice);
+
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей\nСтоимость разработки сайта ' + fullPrice + ' рублей');
